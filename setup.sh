7ce0dace749151dad54c27a77d6be03541be6ec4
numtel <ben@latenightsketches.com>
2024-10-01 22:02:32 -0700

    Use amazon linux 2024 instead of ubuntu

--- setup.sh
+++ setup.sh
@@ -1,14 +1,41 @@
 NARGO_VERSION=`node -e "process.stdout.write(require('../event.json').nargoVersion)"`
-BBUP_VERSION=`node -e "process.stdout.write(require('../event.json').bbupVersion)"`
+export BBUP_VERSION=`node -e "process.stdout.write(require('../event.json').bbupVersion)"`
 
 curl -Lo ./nargo.tar.gz https://github.com/noir-lang/noir/releases/download/v$NARGO_VERSION/nargo-x86_64-unknown-linux-gnu.tar.gz
 tar -xvzf nargo.tar.gz
 chmod +x ./nargo
 mv ./nargo /usr/local/bin
 
-curl -Lo ./bbup.tar.gz https://github.com/AztecProtocol/aztec-packages/releases/download/aztec-packages-v$BBUP_VERSION/barretenberg-x86_64-linux-gnu.tar.gz
-tar -xvzf bbup.tar.gz
-chmod +x ./bb
-mv ./bb /usr/local/bin
+dnf install docker -y
+systemctl start docker
 
-apt install -y libc++-dev libc++abi-dev
+bb55() {
+  docker run --rm  -v "$(pwd)":/app -w /app "numtel/barretenberg:0.55.0" bb "$@"
+}
+export -f bb55
+bb47() {
+  docker run --rm  -v "$(pwd)":/app -w /app "numtel/barretenberg:0.47.1" bb "$@"
+}
+export -f bb47
+bb46() {
+  docker run --rm  -v "$(pwd)":/app -w /app "numtel/barretenberg:0.46.1" bb "$@"
+}
+export -f bb46
+bb41() {
+  docker run --rm  -v "$(pwd)":/app -w /app "numtel/barretenberg:0.41.0" bb "$@"
+}
+export -f bb41
+bb() {
+  if [ "$BBUP_VERSION" == "0.47.1" ]; then
+    bb47 "$@"
+  elif [ "$BBUP_VERSION" == "0.46.1" ]; then
+    bb46 "$@"
+  elif [ "$BBUP_VERSION" == "0.41.0" ]; then
+    bb41 "$@"
+  elif [ "$BBUP_VERSION" == "0.55.0" ]; then
+    bb55 "$@"
+  else
+    echo "Unsupported BBUP_VERSION: $BBUP_VERSION"
+  fi
+}
+export -f bb
